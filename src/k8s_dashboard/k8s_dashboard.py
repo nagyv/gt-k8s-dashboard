f0551acdab6dddc23ed6dbab6f546bb1b0dcdc5e
Viktor Nagy <hello@nagyv.com>
2025-06-01 22:01:33 +0200

    Run dashboard from Pharo code

--- src/k8s_dashboard/k8s_dashboard.py
+++ src/k8s_dashboard/k8s_dashboard.py
@@ -5,9 +5,10 @@ from kubernetes import client, config
 "Use to find how to call more APIs: https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/CoreV1Api.md"
 
 class ListView(ABC):
+	title = "Resources"
+	
 	def __init__(self):
 		self._items = None
-
 
 	@property
 	@abstractmethod
@@ -23,29 +24,65 @@ class ListView(ABC):
 	@gtView
 	def gtViewList(self, aBuilder):
 		table = aBuilder.columnedList()
-		table.title("Namespaces")
+		table.title(self.title)
 		table.priority(10)
 		table.items(lambda: self.items)
 		table.column("Name", lambda item: str(item.metadata.name))
 		table.column("Status", lambda item: str(item.status.phase))
 #		table.column("Age", lambda index: str(self.namespaces[index].metadata.age))
-		table.set_accessor(lambda item: PodList(self.items[item].metadata.name))
+		table.set_accessor(lambda item: Namespace(self.items[item].metadata.name))
 		return table
 
+class NamespacedListView(ListView):
+	def __init__(self, namespace):
+		super().__init__()
+		self._namespace = namespace
+		
 class Namespaces(ListView):
+	title = "Namespaces"
 
 	@property
 	def list(self):
 		v1 = client.CoreV1Api()
 		return v1.list_namespace
 
-class PodList(ListView):
-
-	def __init__(self, namespace):
-		super().__init__()
-		self._namespace = namespace
+class PodList(NamespacedListView):
+	title = "Pods"
 
 	@property
 	def list(self):
 		v1 = client.CoreV1Api()
 		return lambda: v1.list_namespaced_pod(self._namespace)
+		
+class DeploymentList(NamespacedListView):
+	title = "Deployments"
+	
+	@property
+	def list(self):
+		v1 = client.AppsV1Api()
+		return lambda: v1.list_namespaced_deployment(self._namespace)
+		
+class Namespace:
+	
+	def __init__(self, namespace):
+		self.namespace = namespace
+	
+	@gtView
+	def podList(self, aBuilder):
+		table = aBuilder.columnedList()
+		table.title("Pods")
+		table.priority(10)
+		table.items(lambda: client.CoreV1Api().list_namespaced_pod(self.namespace).items)
+		table.column("Name", lambda item: str(item.metadata.name))
+		table.column("Status", lambda item: str(item.status.phase))
+		return table
+		
+	@gtView
+	def deploymentList(self, aBuilder):
+		table = aBuilder.columnedList()
+		table.title("Deployments")
+		table.priority(20)
+		table.items(lambda: client.AppsV1Api().list_namespaced_deployment(self.namespace).items)
+		table.column("Name", lambda item: str(item.metadata.name))
+		table.column("Condition", lambda item: str(item.status.conditions[0].message))
+		return table
